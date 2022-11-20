d89086902768572214fe1cb4f0c4e622cd4273c7
junwei.ye <junwei.ye@weimob.com>
2022-11-20 20:10:27 +0800

    start_dev_server sh update xdebug

--- bin/start_dev_server.sh
+++ bin/start_dev_server.sh
@@ -6,7 +6,7 @@ webRoot=${path}"/../public"
 routePhp=${path}"/../public/index.php"
 
 if [ "$1" = "-d" ]; then
-    php -d variables_order=EGPCS -dxdebug.remote_enable=1 -dxdebug.remote_mode=req -dxdebug.remote_port=9000 -dxdebug.remote_host=127.0.0.1 -dxdebug.remote_autostart=1 -S localhost:18888 -t $webRoot $routePhp
+    php -d variables_order=EGPCS -d xdebug.remote_enable=1 -d xdebug.remote_mode=req -d xdebug.remote_port=9000 -d xdebug.remote_host=127.0.0.1 -d xdebug.remote_autostart=1 -S localhost:18888 -t $webRoot $routePhp
 else
     php -d variables_order=EGPCS -S localhost:18888 -t $webRoot $routePhp
 fi
